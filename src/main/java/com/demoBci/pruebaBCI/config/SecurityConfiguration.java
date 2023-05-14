36c1c3f244186b7a6f08407fd1900dd42c3aac86
KUDAZ <carlos.ccabelloflores@gmail.com>
2023-05-13 20:28:24 -0400

    Ejercicio de java para Bci

--- src/main/java/com/demoBci/pruebaBCI/config/SecurityConfiguration.java
+++ src/main/java/com/demoBci/pruebaBCI/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.demoBci.pruebaBCI.config;
 
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
