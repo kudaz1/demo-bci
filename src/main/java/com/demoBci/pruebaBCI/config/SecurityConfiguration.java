4b9daf413b6bc050e76016575e79456df0f67e5f
KUDAZ <carlos.ccabelloflores@gmail.com>
2023-05-13 20:05:25 -0400

    Ejercicio de java para Bci

--- src/main/java/com/demoBci/pruebaBCI/config/SecurityConfiguration.java
+++ src/main/java/com/demoBci/pruebaBCI/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.demoBci.pruebaBCI.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/demoBci/pruebaBCI/service/IuserService.java
+++ src/main/java/com/demoBci/pruebaBCI/service/IuserService.java
@@ -1,11 +1,9 @@
 package com.demoBci.pruebaBCI.service;
 
 import com.demoBci.pruebaBCI.dto.MensajeApi;
-import com.demoBci.pruebaBCI.dto.UsuarioAutenticar;
 import com.demoBci.pruebaBCI.dto.UsuariosDto;
 import org.springframework.http.ResponseEntity;
 
-import java.util.UUID;
 
 public interface IuserService {
     ResponseEntity<MensajeApi> crear(UsuariosDto usuario);
